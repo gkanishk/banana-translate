e3557ec233cb69eb4f8fceba79e87261db1d4e34
Kanishk Gupta <g.kanishk@holosuit.com>
2020-12-05 10:22:40 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var btn=document.querySelector("#translatebtn");
+var input=document.querySelector("#input-ctn");
+var output=document.querySelector("#output-ctn");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function errorHandler(error) {
+    console.log("something's fishy?", error);
+    alert("something's fishy? try again later")
+}
+
+function convertText(){
+    const url=`${serverURL}?text=${input.value}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(json => {
+            var outputText = json.contents.translated;
+            output.innerText = outputText;
+           })
+        .catch(errorHandler)
+}
+
+btn.addEventListener('click',convertText);
